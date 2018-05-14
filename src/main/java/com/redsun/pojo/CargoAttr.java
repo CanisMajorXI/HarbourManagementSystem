2266942f8091355179bafdf9518a54b7d9290074
RivenDong <1142971273@qq.com>
2018-05-14 17:56:28 +0800

    Merge remote-tracking branch 'remotes/origin/zqw' into lhd
    # Conflicts:
    #	src/main/java/com/redsun/service/impl/UserServiceImpl.java
    #	web/WEB-INF/index.html

---
+++ src/main/java/com/redsun/pojo/CargoAttr.java
@@ -0,0 +1,40 @@
+package com.redsun.pojo;
+
+public class CargoAttr {
+    private Integer typeId;
+    private String name;
+    private Integer maximumInAContainer;
+    private String type;
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getMaximumInAContainer() {
+        return maximumInAContainer;
+    }
+
+    public void setMaximumInAContainer(Integer maximumInAContainer) {
+        this.maximumInAContainer = maximumInAContainer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
